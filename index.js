140d502315cc31229f557fb1c46fbbed4dd54ab6
Vlad <bkotyara@gmail.com>
2023-07-07 23:48:26 +0300

    Challenge Project API

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ require('productionize')(name)
 var server = require('./lib/server')
 var port = process.env.PORT || 5000
 server().listen(port)
-console.log(name, 'listening on port', port)
+console.log(name, 'listening on port', port, 'NODE_ENV', process.env.NODE_ENV)
--- test/endpoints.js
+++ test/endpoints.js
@@ -15,3 +15,72 @@ test.serial.cb('healthcheck', function (t) {
     t.end()
   })
 })
+
+// https://github.com/rvagg/archived-servertest/pull/11
+const sampleTarget = {
+  "id": "1",
+  "url": "http://example.com",
+  "value": "0.50",
+  "maxAcceptsPerDay": "10",
+  "accept": {
+    "geoState": {
+      "$in": ["ca", "ny"]
+    },
+    "hour": {
+      "$in": [ "13", "14", "15" ]
+    }
+  }
+}
+
+const updatedTarget = {
+  "id": "1",
+  "url": "https://google.com",
+  "value": "0.50",
+  "maxAcceptsPerDay": "10",
+  "accept": {
+    "geoState": {
+      "$in": ["ca", "ny"]
+    },
+    "hour": {
+      "$in": [ "13", "14", "15" ]
+    }
+  }
+}
+
+test.serial.cb('Making a decision', t => {
+  servertest(server(), '/api/targets', { method: 'POST', json: sampleTarget }, async (err, res) => {
+    t.is(res.statusCode, 200)
+    t.deepEqual(res.body, sampleTarget)
+    t.end()
+  });
+
+  servertest(server(), '/api/targets/1', { method: 'GET' }, function (err, res) {
+    t.is(res.statusCode, 200)
+    t.deepEqual(res.body, sampleTarget)
+    t.end()
+  });
+
+  servertest(server(), '/api/targets', { method: 'GET' }, function (err, res) {
+    t.is(res.statusCode, 200)
+    t.deepEqual(res.body, [sampleTarget])
+    t.end()
+  });
+
+  servertest(server(), '/api/targets/1', { method: 'POST', json: updatedTarget }, async (err, res) => {
+    t.is(res.statusCode, 200)
+    t.deepEqual(res.body, updatedTarget)
+    t.end()
+  });
+
+  servertest(server(), '/route', { method: 'POST', json: {
+      "geoState": "ny",
+      "publisher": "abc",
+      "timestamp": new Date().toISOString()
+    }}, async (err, res) => {
+    t.is(res.statusCode, 200)
+    t.is(res.body.status, 'OK', 'status is ok');
+    t.end()
+  });
+
+
+});
